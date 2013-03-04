6efcf3ee282b1897888faede270cc79d9fb009f4
Steph McArthur <stephmc.tdu@gmail.com>
2013-03-04 14:56:44 +1100

    getting auto pull on git to work correctly

--- updateRepos.sh
+++ updateRepos.sh
@@ -1,15 +1,18 @@
 #! bash
 # Needs to take in the sha1 to update head to
+
 echo starting update
 #cd ../web-animations-js
 #git pull
-cd ../web-animations-test-framework
+cd ..
+git clone https://github.com/StephMc/web-animations-test-framework.git
+cd web-animations-test-framework
 git pull
 
 if [ "$1" != "" ];
 then
   echo "reseting git to $1"
-  git reset --soft $1
+  #git reset --soft $1
 fi
 
 echo finished
